b9ea9bbe7705591d7e4f2724b422fa12001b4988
Gleb <gleb.gadbrat@mail.ru>
2022-03-05 15:00:16 +0300

    задание 9 - Найти третью цифру числа или сообщить, что её нет

---
+++ Tasks/Task9/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 100)
+{
+    while (a >= 1000)
+    {
+        a /= 10;
+    }
+    a %= 10;
+    Console.Write("Третья цифра числа - ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("У числа отсутсвует третья цифра");
+}
