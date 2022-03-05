1bc1a942ad6812052dce33edf17ec931fbb008b5
Gleb <gleb.gadbrat@mail.ru>
2022-03-05 13:59:29 +0300

    задание 7 - Удалить вторую цифру трёхзначного числа

---
+++ Tasks/Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int ost = 0;
+while (a >= 100)
+{
+    ost = ost + ((a % 10) * count);
+    count *= 10;
+    a /= 10;
+}
+a = a/10 * count + ost;
+Console.Write("Получишееся число: ");
+Console.WriteLine(a);
