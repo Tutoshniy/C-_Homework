027264daf29cabf4b75208abac8377e491830ca3
Gleb <gleb.gadbrat@mail.ru>
2022-03-05 14:39:06 +0300

    задание 8 - Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ Tasks/Task8/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a >= b)
+{
+    if (a % b == 0)
+    {
+        Console.Write("Кратность найдена и она равна: ");
+        Console.WriteLine(a / b);
+    }
+    else
+    {
+        Console.Write("Кратность отсутствует. Остаток от деления равен: ");
+        Console.WriteLine(a % b);
+    }
+}
+if (a < b)
+{
+    if (b % a == 0)
+    {
+        Console.Write("Кратность найдена и она равна: ");
+        Console.WriteLine(b / a);
+    }
+    else
+    {
+        Console.Write("Кратность отсутствует. Остаток от деления равен: ");
+        Console.WriteLine(b % a);
+    }
+}
