c087258f47dbf0dd11dfe4dbc77bd4fa17ea61e0
Gleb <gleb.gadbrat@mail.ru>
2022-03-05 12:09:43 +0300

    задание 4 - Показать последнюю цифру трёхзначного числа

---
+++ Tasks/Task4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Последняя цифра числа: ");
+Console.WriteLine(a%10);
