e65814bc326b509a72a57e729bd12c62bb1705bf
Gleb <gleb.gadbrat@mail.ru>
2022-03-05 12:50:17 +0300

    задание 5 - Показать вторую цифру трёхзначного числа

---
+++ Tasks/Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a >= 100)
+{
+    a = a / 10;
+}
+a = a % 10;
+Console.Write("Вторая цифра трёхзначного числа - ");
+Console.WriteLine(a);
